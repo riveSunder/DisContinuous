b9be8a00fcdba88c33cc5eb1efbbc147c703be79
Your Name <you@example.com>
2023-06-23 08:18:59 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(\
+        name="DisContinuous", \
+        packages=["disco"], \
+        version = "0.0", \
+        description = "DisContinuous Cellular Automata", \
+    )
+
+
