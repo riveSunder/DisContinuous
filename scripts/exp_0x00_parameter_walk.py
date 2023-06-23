ac5df22b565c91c66e2988d17c1d569e623f74f0
Your Name <you@example.com>
2023-06-23 11:46:24 -0600

    unbreak mkdir logic in experiment scripts, and define log_directory properly

--- scripts/exp_0x00_parameter_walk.py
+++ scripts/exp_0x00_parameter_walk.py
@@ -1,13 +1,14 @@
 import os
 
-import pdb; pdb.set_trace()
-script_path = os.path.split(os.path.abspath(__file__))[0]
-log_directory = os.path.join(script_path, "..", "logs", "exp_0x00")
+script_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+log_directory = os.path.join(script_path, "logs", "exp_0x00")
 log_filepath_nca = os.path.join(log_directory, "exp_0x00_nca_save_image_log.txt")
 log_filepath_cca = os.path.join(log_directory, "exp_0x00_cca_save_image_log.txt")
 
 if os.path.exists(log_directory):
-	os.mkdir(log_directory)
+    pass
+else:
+    os.mkdir(log_directory)
 
 time_limit = 14400
 system_type = "NCA"
@@ -37,5 +38,5 @@ my_cmd_cca = f"python -m disco.walk -l {time_limit} -c {system_type_cca} "\
         f"-g {glider_patterns_cca}"\
         f"-s {save_images} -m {max_steps} -d {device} -k {kernel_radius_bounds} > {log_filepath_cca}"
 
-os.system(my_cmd)
+os.system(my_cmd_nca)
 os.system(my_cmd_cca)
