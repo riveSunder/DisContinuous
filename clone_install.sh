e8e55a7be905f72975c459949b90afc2b65dd1d2
Your Name <you@example.com>
2023-06-23 11:47:46 -0600

    update git commit reference for yuca in clone_install.sh script

--- clone_install.sh
+++ clone_install.sh
@@ -1,4 +1,4 @@
 git clone --shallow-since 2022-07-28 git@github.com:riveSunder/yuca.git
 cd yuca 
-git checkout b91ef7ab9cf468fda9a623fe88cac9c0623633bf
+git checkout d6bca874d986e2838ae272edbeba833548cbf042 
 pip install -e .
